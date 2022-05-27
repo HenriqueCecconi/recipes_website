92aaea75a2108f6e270150b73bb6c9a951bf9109
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-05-27 14:42:22 -0300

    Adding a simple recipe to my Template. Also fixing yet more typos.

--- recipes_website/urls.py
+++ recipes_website/urls.py
@@ -1,4 +1,4 @@
-"""repicies_website URL Configuration
+"""recipes_website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
