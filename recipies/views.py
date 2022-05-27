ea43df241fb44f7d6010401ae3bb37b809200cee
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-05-26 23:11:34 -0300

    Setting up a very basic View and my Urls for the Recipies app. Also adding a missing comma in settings.py that was breaking the server.

--- recipies/views.py
+++ recipies/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Recipies</h1> <h2>Welcome tho this basic recipies website</h2>')
--- repicies_website/settings.py
+++ repicies_website/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'recipies'
+    'recipies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
